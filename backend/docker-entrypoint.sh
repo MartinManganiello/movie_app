767e11804b10afb65907e8ac43443c92027dafce
Martín Manganiello <francomartinmanganiello@gmail.com>
2020-06-16 19:11:00 -0300

    add entrypoint.sh

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+echo "Waiting for postgres..."
+
+for i in 1 2 3 4 5
+do
+  sleep 0.1
+done
+
+echo "PostgreSQL started"
+
 python manage.py flush --no-input
 # PGPASSWORD=djangoproject psql --host db --port 5432 --username=code.djangoproject --dbname=code.djangoproject < tracdb/trac.sql
 python manage.py migrate
