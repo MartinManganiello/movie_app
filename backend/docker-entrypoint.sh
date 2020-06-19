bb07a8ff2c20c2d27b16ed0df9c6541847318a31
Martín Manganiello <francomartinmanganiello@gmail.com>
2020-06-19 12:22:50 -0300

    Commented flush

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -9,7 +9,8 @@ done
 
 echo "PostgreSQL started"
 
-python manage.py flush --no-input
+#Clean data
+# python manage.py flush --no-input
 # PGPASSWORD=djangoproject psql --host db --port 5432 --username=code.djangoproject --dbname=code.djangoproject < tracdb/trac.sql
 python manage.py migrate
 # python manage.py collectstatic --no-input --clear
