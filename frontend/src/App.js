31423246703623081f8a45e73385a69924162e26
Martín Manganiello <francomartinmanganiello@gmail.com>
2020-06-22 13:43:57 -0300

    Fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
   }
 
   loadMovie = movie => {
-    const elementsIndex = this.state.movies.findIndex(element => element.id == movie.id)
+    const elementsIndex = this.state.movies.findIndex(element => element.id === movie.id)
     let newArray = [...this.state.movies];
     newArray[elementsIndex] = {...newArray[elementsIndex], avg_rating: movie.avg_rating}
     this.setState({ selectedMovie: movie, editedMovie: null, movies: newArray})
